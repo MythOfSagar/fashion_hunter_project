f7c9f578a4c570b8e44fec24f3a7f94bbc87aeca
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-18 21:32:55 +0530

    file

---
+++ frontend/src/DurgeshFolder/work.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const work = () => {
+  return (
+    <div>
+       <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default work
+//  Dummy work -----------------------------------
