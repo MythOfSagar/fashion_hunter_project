5226024e1020354a674b2c4f9c4d3810c2977cd5
Durgesh9871 <devisingh9871686098@gmail.com>
2023-01-28 20:10:12 +0530

    file

--- frontend/src/Redux/Review_reducer/action.js
+++ frontend/src/Redux/Review_reducer/action.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const getReviewData =(num )=> (dispatch)=>{
        dispatch({type:"REVIEW_DATA_LOADING"})
-       return axios.get(`https://graceful-lion-wig.cyclic.app/review/add${num}`)
+       return axios.get(`https://handsome-blue-crab.cyclic.app/review/add${num}`)
        .then((res)=>dispatch({type:"REVIEW_DATA_SUCCESS" , payload:res.data}) ,dispatch({type:"REVIEW_DATA_SUCCESS_Number" , payload:num}))
        .catch((err)=>dispatch({type:"REVIEW_DATA_ERROR"}))
 
