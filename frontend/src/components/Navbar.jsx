df4fe2b259c123ecbb6227744170713f7289fc4c
Hilton Borah <hiltonborah123@gmail.com>
2023-02-17 16:11:59 +0530

    update navbar and authentication in cart page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import useWindowsize from "../Hook/HandW.hook";
 import "./Navbar.css";
 import DraweExample from "./dwrawer"
-import {FaHeart} from "react-icons/fa"
-import { BsFillHandbagFill} from "react-icons/bs";
+import { FaHeart } from "react-icons/fa"
+import { BsFillHandbagFill } from "react-icons/bs";
 import Action from "./action"
 import { Link, useNavigate } from "react-router-dom";
-import { InputGroup, InputLeftElement , Input, useToast, Box } from "@chakra-ui/react";
+import { InputGroup, InputLeftElement, Input, useToast, Box } from "@chakra-ui/react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { getLocalData } from "../Utils/LocalStorage";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getCartData } from "../Redux/Cart_reducer/action"; 
+import { getCartData } from "../Redux/Cart_reducer/action";
 import { getWishListData } from "../Redux/Wishlist_reducer/action";
 
 
 const Navbar = () => {
   const dispatch = useDispatch()
-  const cartArrayData = useSelector(state=>state.CartReducer.cartArrayData)
-  const wishListArray = useSelector(state=>state.WishlistReducer.wishListArray)
-  
-  const [width]=useWindowsize();
+  const cartArrayData = useSelector(state => state.CartReducer.cartArrayData)
+  const wishListArray = useSelector(state => state.WishlistReducer.wishListArray)
+
+  const [width] = useWindowsize();
   const navigate = useNavigate()
   const toast = useToast()
 
-  const handleLogin=()=>{
-    if (!getLocalData("token")){
+  const handleLogin = () => {
+    if (!getLocalData("token")) {
       navigate("/login")
       toast({
         title: `You are not login, login first`,
@@ -36,33 +36,33 @@ const Navbar = () => {
   }
 
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getCartData)
     dispatch(getWishListData)
 
-  },[])
+  }, [])
 
   return (
     <div id="navbar">
-   <div id="navbar-left">
-   <Link to="/">   <img
+      <div id="navbar-left">
+        <Link to="/">   <img
           id="site-logo"
           src="https://i.ibb.co/2dkyD2f/Untitledfdgfdg.png"
           alt="home page logo"
         /></Link>
       </div>
-      
-      
-  { width>900 ?
+
+
+      {/* { width>900 ? */}
       <div id="navbar-right">
         <div id="right-upper">
-            <p id="ru">Customer Care</p>
+          <p id="ru">Customer Care</p>
           <button id="navbar-button">Visit AJIOLUXE</button>
-          <Action/>
-           
+          <Action />
+
         </div>
         <div id="right-lower">
-         <Link to="/mens"> <p id="rl">MEN</p></Link>
+          <Link to="/mens"> <p id="rl">MEN</p></Link>
           <p id="rl">WOMEN</p>
           <p id="rl">KIDS</p>
           <p id="rl">INDIE</p>
@@ -70,36 +70,36 @@ const Navbar = () => {
 
           {/*  Search bar ------------------------------------ */}
           {/* <input id="navbar-input" type="search"  placeholder="Search Products" style={{outline:"none", borderRadius:"4px"}} /> */}
-         <Link to="/search"> <InputGroup>
-    <InputLeftElement cursor="pointer" pointerEvents='none' children={<AiOutlineSearch color='gray.300' />} t/>
-    <Input type='tel' placeholder='Search Products'  variant="" border="1px solid #2f4254" width="200px"/>
-  </InputGroup> </Link>
-  {/*  Search bar end ----------------------- */}
+          <Link to="/search"> <InputGroup>
+            <InputLeftElement cursor="pointer" pointerEvents='none' children={<AiOutlineSearch color='gray.300' />} t />
+            <Input type='tel' placeholder='Search Products' variant="" border="1px solid #2f4254" width="200px" />
+          </InputGroup> </Link>
+          {/*  Search bar end ----------------------- */}
 
           <div className="icon" onClick={handleLogin}>
             {/*  Wishlist -------------durgesh */}
             <Box className="icon-div" position="relative">
-             <Link to="/wishlist"> <FaHeart /> </Link>
-          { (wishListArray[0] !== "P" && wishListArray.length !== 0) &&  <Box className="wishListClass" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%"  fontSize="18px" border="1px solid #ffffff">{wishListArray.length}</Box> }
+              <Link to="/wishlist"> <FaHeart /> </Link>
+              {(wishListArray[0] !== "P" && wishListArray.length !== 0) && <Box className="wishListClass" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%" fontSize="18px" border="1px solid #ffffff">{wishListArray.length}</Box>}
 
 
             </Box>
 
             {/*  cart ------------- durgesh ----*/}
-            <Box className="icon-div" position="relative" > 
-            <Link to="/cart"> <Box><BsFillHandbagFill /></Box></Link>
-               
-            {(cartArrayData[0] !== "P" && cartArrayData.length !== 0) && <Box className="manoj" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%"  fontSize="18px" border="1px solid #ffffff" >{ cartArrayData.length }</Box> }
+            <Box className="icon-div" position="relative" >
+              <Link to="/cart"> <Box><BsFillHandbagFill /></Box></Link>
+
+              {(cartArrayData[0] !== "P" && cartArrayData.length !== 0) && <Box className="manoj" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%" fontSize="18px" border="1px solid #ffffff" >{cartArrayData.length}</Box>}
             </Box>
             {/* main end ------------------- */}
           </div>
         </div>
       </div> :
-         <div className="draw">
+      <div className="draw">
 
-           <DraweExample />
-         </div>
-      }
+        <DraweExample />
+      </div>
+      {/* // } */}
     </div>
   );
 };
--- frontend/src/components/dwrawer.jsx
+++ frontend/src/components/dwrawer.jsx
@@ -1,52 +1,103 @@
-import { Box, Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, FormLabel, Input, InputGroup, InputLeftAddon, InputRightAddon, Stack, Textarea, useDisclosure } from "@chakra-ui/react"
-import React from "react"
-import {HamburgerIcon } from '@chakra-ui/icons'
+import { Box, Button, Drawer, DrawerBody, DrawerCloseButton,useToast, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, FormLabel, Input, InputGroup, InputLeftAddon, InputRightAddon, Stack, Textarea, useDisclosure } from "@chakra-ui/react"
+import React, { useEffect } from "react"
+import { HamburgerIcon } from '@chakra-ui/icons'
 import { AiOutlineHeart } from "react-icons/ai";
 import { BsHandbag } from "react-icons/bs";
 import Action from "./action";
+import { Link, useNavigate } from "react-router-dom";
+import { getLocalData } from "../Utils/LocalStorage";
+import {FaHeart} from "react-icons/fa"
+import { BsFillHandbagFill} from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { getWishListData } from "../Redux/Wishlist_reducer/action";
+import { getCartData } from "../Redux/Cart_reducer/action";
+
 
 function DrawerExample() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const firstField = React.useRef()
-  
-    return (
-      <>
-        <Button color={"white"} bgColor={"#2f4254"}variant={"unstyled"} textAlign="center" onClick={onOpen}><HamburgerIcon/>
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='right'
-          initialFocusRef={firstField}
-          onClose={onClose}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-           
-            <DrawerBody>
-              <Stack spacing='24px'>
-              <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={20} gap={10}>
-             <Action/>
-        <a href="#">Men</a>
-        <a href="#">Women</a>
-        <a href="#" >KIDS</a>
-        <a href="#">INDIE</a>
-        <a href="#">Home AND KITCHEN</a>
-        <div className="icon">
-            <div className="icon-div">
-              <AiOutlineHeart />
-            </div>
-            <div className="icon-div"> 
-              <BsHandbag />
-            </div>
-          </div>
-      </Box>
+  const navigate = useNavigate();
+  const toast = useToast()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const firstField = React.useRef();
 
-              </Stack>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
+  const dispatch = useDispatch()
+  const cartArrayData = useSelector(state=>state.CartReducer.cartArrayData)
+  const wishListArray = useSelector(state=>state.WishlistReducer.wishListArray)
+
+  const handleLogin=()=>{
+    if (!getLocalData("token")){
+      navigate("/login")
+      toast({
+        title: `You are not login, login first`,
+        status: 'error',
+        isClosable: true,
+      })
+    }
   }
-  export default DrawerExample
+
+  
+  useEffect(()=>{
+    dispatch(getCartData)
+    dispatch(getWishListData)
+
+  },[])
+
+
+
+  return (
+    <>
+      <Button color={"#2f4254"} fontSize={"30px"} variant={"unstyled"} textAlign="center" onClick={onOpen}><HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        initialFocusRef={firstField}
+        onClose={onClose}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          <DrawerBody>
+            <Stack spacing='24px'>
+              <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={20} gap={10}>
+                <Action />
+                <Link to={"/mens"}>Men</Link>
+                <a href="#">Women</a>
+                <a href="#" >KIDS</a>
+                <a href="#">INDIE</a>
+                <a href="#">Home AND KITCHEN</a>
+                {/* <div className="icon">
+                  <div className="icon-div">
+                    <Link to={"/wishlist"}><AiOutlineHeart /></Link>
+                  </div>
+                  <div className="icon-div">
+                    <Link to={"/cart"}><BsHandbag /></Link>
+                  </div>
+                </div> */}
+                <div className="icon" onClick={handleLogin}>
+                  {/*  Wishlist -------------durgesh */}
+                  <Box className="icon-div" position="relative">
+                    <Link to="/wishlist"> <FaHeart /> </Link>
+                    {(wishListArray[0] !== "P" && wishListArray.length !== 0) && <Box className="wishListClass" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%" fontSize="18px" border="1px solid #ffffff">{wishListArray.length}</Box>}
+
+
+                  </Box>
+
+                  {/*  cart ------------- durgesh ----*/}
+                  <Box className="icon-div" position="relative" >
+                    <Link to="/cart"> <Box><BsFillHandbagFill /></Box></Link>
+
+                    {(cartArrayData[0] !== "P" && cartArrayData.length !== 0) && <Box className="manoj" position="absolute" left="22px" top="-2px" color="#ffffff" backgroundColor="#ff6161" width="23px" height="25px" fontWeight="700" borderRadius="50%" fontSize="18px" border="1px solid #ffffff" >{cartArrayData.length}</Box>}
+                  </Box>
+                  {/* main end ------------------- */}
+                </div>
+              </Box>
+
+            </Stack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+export default DrawerExample
